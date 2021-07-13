e7aefcd35fd20e641d6dc6e683f15ffacfb3554a
jun <j2junn@gmail.com>
2021-07-13 23:19:08 +0900

    2021.07.13 1개 완료

---
+++ study/UnionFind.py
@@ -0,0 +1,59 @@
+import sys
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+    
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+        
+        
+
+# 노드의 개수와 간선(Union 연산)의 개수 입력 받기
+v, e = map(int, sys.stdin.readline().split())
+
+# 부모 테이블 초기화하기
+# 부모 테이블에서, 부모를 자기 자신으로 초기화
+parent = [i for i in range(v + 1)]
+
+cycle = False
+
+# Union 연산을 각각 수행
+for i in range(e):
+    a, b = map(int, sys.stdin.readline().split())
+    
+    # 사이클이 발생한 경우 종료
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    # 사이클이 발생하지 않았다면 합집합(Union) 연산 수행
+    else:
+        union_parent(parent, a, b)
+        
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
+
+# 각 원소가 속한 집합 출력하기
+print("각 원소가 속한 집합: ", end='')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end='')
+
+print()
+
+# 부모 테이블 내용 출력하기
+print('부모 테이블: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end='')
